45c5314f934b8f39cf60abb7ce3ba3dd8ee0f6c8
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-05-18 01:13:20 +0200

    add thumbnail for the background video

--- src/lib/components/BackgroundVideo.svelte
+++ src/lib/components/BackgroundVideo.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import backgroudVideoAvc from "$lib/videos/background_video.avc.mp4"
+    import thumbnail from "$lib/images/video-thumnail.png"
     import { onMount } from "svelte";
 
     let video: HTMLVideoElement;
@@ -10,5 +11,5 @@
 </script>
 
 <div class="{$$restProps.class} after:absolute after:size-full after:top-0 after:left-0 after:backdrop-blur-[80px]">
-    <video bind:this={video} class="size-full object-cover" src="{backgroudVideoAvc}" autoplay loop muted defaultMuted preload="auto">
+    <video bind:this={video} class="size-full object-cover" src="{backgroudVideoAvc}" autoplay loop muted defaultMuted preload="auto" poster="{thumbnail}">
 </div>
