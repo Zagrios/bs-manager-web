4688d6bd45a211e0a5a63a224b302baae883bb85
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-05-18 01:03:38 +0200

    remove useless things

--- src/lib/components/BackgroundVideo.svelte
+++ src/lib/components/BackgroundVideo.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import backgroudVideo from "$lib/videos/background_video.av1.mp4"
+    import backgroudVideoAvc from "$lib/videos/background_video.avc.mp4"
     import { onMount } from "svelte";
 
     let video: HTMLVideoElement;
@@ -10,5 +10,5 @@
 </script>
 
 <div class="{$$restProps.class} after:absolute after:size-full after:top-0 after:left-0 after:backdrop-blur-[80px]">
-    <video bind:this={video} class="size-full object-cover" src={backgroudVideo} autoplay muted loop></video>
+    <video bind:this={video} class="size-full object-cover" src="{backgroudVideoAvc}" autoplay loop muted defaultMuted preload="auto">
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import BackgroundVideo from "$lib/components/BackgroundVideo.svelte";
-	import Particles from "$lib/components/Particles.svelte";
 	import discordIcon from "$lib/images/discord-mark-white.svg";
 	import xIcon from "$lib/images/x-logo.svg";
 	import ghIcon from "$lib/images/github-icon.svg";
 	import patreonIcon from "$lib/images/patreon-icon.svg";
-	import beatRunning from "$lib/images/beat_running.gif";
+	import beatRunning from "$lib/images/beat_running.gif";	
 
+	let smallDevice = true;
 	let iconElement: HTMLImageElement;
 	let iconContainer: HTMLDivElement;
 
@@ -32,10 +32,9 @@
 	<meta name="description" content="BSManager website" />
 </svelte:head>
 
-<section class="w-screen h-screen overflow-hidden">
+<section class="w-dvw h-dvh overflow-hidden">
 
 	<BackgroundVideo class="size-full absolute top-0 left-0 -z-10"/>
-	<Particles class="absolute size-full -z-10" />
 	<div class="size-full flex justify-center items-center flex-col z-30 gap-5 px-3 [&>*]:max-w-full">
 		<div bind:this={iconContainer} on:mousemove={magnetEffect} on:mouseleave={magnetEffect} class="size-48 sm:size-96 max-h-full max-w-full" role="form">
 			<img bind:this={iconElement} id="bsm-icon" class="size-full shadow-black drop-shadow-md ease-in-out duration-300" src="favicon.svg" alt="BSManager icon"/>
@@ -60,14 +59,13 @@
 				<img class="size-full" src="{patreonIcon}" alt="patreon icon"/>
 			</a>
 		</section>
-		<p class="text-gray-400 whitespace-break-spaces text-center mt-10">Add this site to your favorites and stay tuned! This is work in progress, and for now, it is used only as temporary content.</p>
+		<p class="text-gray-400 whitespace-break-spaces text-center mt-3 sm:mt-10">Add this site to your favorites and stay tuned! This is work in progress, and for now, it is used only as temporary content.</p>
 	</div>
 
 	<div class="absolute bottom-4 right-4 text-neutral-100 flex justify-center items-center gap-3 *:underline *:underline-offset-2">
 		<a href="https://beatsaver.com/maps/16992" target="_blank">See map</a>
 		<a href="beatsaver://16992">Install map</a>
 	</div>
-
 
 </section>
 
